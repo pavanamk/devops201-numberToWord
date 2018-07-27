6ee69c6edfbe28164232dc6eda77d89cc0ba9d39
Krithiupadhya <krithiur@gmail.com>
2018-07-27 20:56:18 +0530

    Updating to read user input

--- src/com/mindtree/main/ConvertNumberMain.java
+++ src/com/mindtree/main/ConvertNumberMain.java
@@ -3,6 +3,8 @@
  */
 package com.mindtree.main;
 
+import java.util.Scanner;
+
 import com.mindtree.service.ConvertNumber;
 
 /**
@@ -16,24 +18,30 @@ public class ConvertNumberMain {
 	 */
 	public static void main(String[] args) {
 
-		if(args.length<1) {
-			System.out.println("Atlease a single argument should be passed!!");
-			return;
-		}
-		
+		System.out.println("---Starting number conversion to word---");
 		try {
-			System.out.println("Starting conversion");
-			for (int i = 0; i < args.length; i++) {
-				int number= Integer.parseInt(args[i]);
-				System.out.println(args[i]+"- "+ConvertNumber.getNumberToWord(number));
+			Scanner scanner=new Scanner(System.in);
+			int input = 0;
+			System.out.print("Enter a number(1-999) to convert(0 to exit): ");
+			input=scanner.nextInt();
+			while(input!=0) {
+				if(input<1 || input>999) {
+					System.out.print("Invalid argument passed.");
+				}
+				System.out.println("Result- "+ConvertNumber.getNumberToWord(input));
+				
+				System.out.print("Enter a number(1-999) to convert(0 to exit): ");
+				input = scanner.nextInt();
 			}
+			
 		} catch (NumberFormatException e) {
-			e.printStackTrace();
+			//e.printStackTrace();
 			System.err.println("Invalid argument passed. Please verify the arguments!!");
 		} catch (Exception e) {
-			e.printStackTrace();
+			//e.printStackTrace();
 			System.err.println("Error while converting number to word!!");
 		}
+		System.out.println("---Exiting number conversion to word---");
 	}
 
 }
